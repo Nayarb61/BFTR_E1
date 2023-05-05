01db2f6ef5ab0f77f4bf754b5fe87e4daebc8f73
Nayarb61 <nayarbtenorio@hotmail.com>
2023-05-05 15:12:33 -0600

    Primer Commit

---
+++ app/src/main/java/com/example/bftr_e1/Formulario.kt
@@ -0,0 +1,20 @@
+package com.example.bftr_e1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.DatePicker
+
+class Formulario : AppCompatActivity() {
+  //  private lateinit var datePicker: DatePicker
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario)
+
+    //    datePicker = findViewById(R.id.datePicker)
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/bftr_e1/PDatos.kt
@@ -0,0 +1,11 @@
+package com.example.bftr_e1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PDatos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pdatos)
+    }
+}
